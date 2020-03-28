1e3ae72f48f7f1dc85816a0cde8e610bc079f36d
Natan Foleto <39577730+natanfoleto@users.noreply.github.com>
2020-03-28 13:56:14 -0300

    Aplicação finalizada!

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://natanfoleto:143010@cluster0-j8wn2.mongodb.net/godev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
